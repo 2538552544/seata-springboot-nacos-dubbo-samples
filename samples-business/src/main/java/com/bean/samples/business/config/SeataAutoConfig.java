3ab3d6092af75aac073d453664e77cc75b49ac62
bean <2538552544@github.com>
2023-05-03 23:06:44 +0800

    1:seata从1.4.1升级成1.6.1保持和服务端版本一致

--- samples-business/src/main/java/com/bean/samples/business/config/SeataAutoConfig.java
+++ samples-business/src/main/java/com/bean/samples/business/config/SeataAutoConfig.java
@@ -15,9 +15,18 @@
  */
 package com.bean.samples.business.config;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import io.seata.spring.annotation.GlobalTransactionScanner;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.transaction.SpringManagedTransactionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
 
 /**
  * @Author: heshouyou
@@ -26,5 +35,55 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class SeataAutoConfig {
+
+    /**
+     * autowired datasource config
+     */
+    @Autowired
+    private DataSourceProperties dataSourceProperties;
+
+    /**
+     * init durid datasource
+     *
+     * @Return: druidDataSource  datasource instance
+     */
+    @Bean
+    public DruidDataSource druidDataSource() {
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setUrl(dataSourceProperties.getUrl());
+        druidDataSource.setUsername(dataSourceProperties.getUsername());
+        druidDataSource.setPassword(dataSourceProperties.getPassword());
+        druidDataSource.setDriverClassName(dataSourceProperties.getDriverClassName());
+        druidDataSource.setInitialSize(0);
+        druidDataSource.setMaxActive(180);
+        druidDataSource.setMaxWait(60000);
+        druidDataSource.setMinIdle(0);
+        druidDataSource.setValidationQuery("Select 1 from DUAL");
+        druidDataSource.setTestOnBorrow(false);
+        druidDataSource.setTestOnReturn(false);
+        druidDataSource.setTestWhileIdle(true);
+        druidDataSource.setTimeBetweenEvictionRunsMillis(60000);
+        druidDataSource.setMinEvictableIdleTimeMillis(25200000);
+        druidDataSource.setRemoveAbandoned(true);
+        druidDataSource.setRemoveAbandonedTimeout(1800);
+        druidDataSource.setLogAbandoned(true);
+        return druidDataSource;
+    }
+
+    /**
+     * init mybatis sqlSessionFactory
+     *
+     * @Param: dataSourceProxy  datasource proxy
+     * @Return: DataSourceProxy  datasource proxy
+     */
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setMapperLocations(
+                new PathMatchingResourcePatternResolver().getResources("classpath*:/mapper/*.xml"));
+        factoryBean.setTransactionFactory(new SpringManagedTransactionFactory());
+        return factoryBean.getObject();
+    }
 
 }
